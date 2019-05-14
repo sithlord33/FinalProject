24b9ce1df131ebd00a70916515ec48709ee80e60
Ricardo Jimenez <ricardotodd33@hotmail.com>
2019-05-13 19:14:37 -0600

    added initial agents

--- src/tournament/GameMaster.java
+++ src/tournament/GameMaster.java
@@ -32,12 +32,11 @@ public class GameMaster {
 	public static void main(String[] args) {
 		ArrayList<Player> players = new ArrayList<Player>();
 		//players.add(new UniformRandom());
-		//players.add(new SolidRock());
-		players.add(new ManualOverride());
-		players.add(new MaxMinPayoff());
-		players.add(new MinMaxRegret());
+		players.add(new SolidRock());
+		//players.add(new ManualOverride());
+		//players.add(new MaxMinPayoff());
+		//players.add(new MinMaxRegret());
 		players.add(new PSNE());
-		//add your agent(s) here
 		
 		ArrayList<Parameters> settings = new ArrayList<Parameters>();
 		//settings.add(new Parameters(maxPayoff,numActions,0,0,0,GameType.ZERO_SUM));
